994977137f52246674057ba58412816e56205e0d
the-bug-lover <aleksander24sfj@gmail.com>
2024-11-06 09:22:54 +0100

    commented out irrelevant code

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function addADoshin() {
   let randNum = Math.ceil(Math.random() * 5);
   let currentDoshin = 0;
 
-  let extraPics = 0;
-  if (prevCubeSize != 1) extraPics = cubeSize * cubeSize - doshins;
+  // let extraPics = 0;
+  // if (prevCubeSize != 1) extraPics = cubeSize * cubeSize - doshins;
 
   doshins++;
 
@@ -17,7 +17,7 @@ function addADoshin() {
       cubeSize * cubeSize
     } doshins!`
   );
-  console.log(extraPics);
+  // console.log("There are " + extraPics + " extra pics");
 
   id.innerHTML += `<img class="doshin" src="../images/doshin${randNum}.png" alt=""/>`;
 
@@ -28,29 +28,34 @@ function addADoshin() {
 
   let height = prevCubeSize;
   let width = 0;
-  let row = 1;
   let rowSize = cubeSize;
+  // let row = 1;
 
   document.querySelectorAll(".doshin").forEach((element) => {
     currentDoshin++;
+    if (Math.ceil(doshins / height) > rowSize) {
+      console.log("Row Pass");
+      rowSize++;
+    }
 
     // if(currentDoshin > rowSize * row && ) rowSize;
-    if (Math.ceil(doshins / height) > rowSize) rowSize++;
 
-    console.log(`THE ROW SIZE IS ${rowSize}`);
+    // console.log(`THE ROW SIZE IS ${rowSize}`);
 
     if (doshins % prevCubeSize == 0) {
       console.log("PASS ONE");
       width = doshins / prevCubeSize;
-    } else if (extraPics > 0 && currentDoshin >= rowSize * row) {
-      console.log("PASS TWO");
-      width = rowSize;
-      row++;
-      extraPics--;
     } else {
       console.log("PASS THREE");
       width = rowSize;
     }
+
+    // else if (extraPics > 0 && currentDoshin >= rowSize * row) {
+    //   console.log("PASS TWO");
+    //   width = rowSize;
+    //   row++;
+    //   extraPics--;
+    // }
 
     // else if (extraPics > 0 && currentDoshin > cubeSize * row) {
     //   width = cubeSize;
