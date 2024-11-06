e090fdd22d1ee8ff410f46f99f5a20cd55c38d60
the-bug-lover <aleksander24sfj@gmail.com>
2024-11-06 08:50:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const id = document.getElementById("container");
+let doshins = 0;
+let cubeSize = 2;
+let prevCubeSize = 1;
+
+function addADoshin() {
+  let randNum = Math.ceil(Math.random() * 5);
+  let currentDoshin = 0;
+
+  let extraPics = 0;
+  if (prevCubeSize != 1) extraPics = cubeSize * cubeSize - doshins;
+
+  doshins++;
+
+  console.log(
+    `There are ${doshins} doshins and the square is ${
+      cubeSize * cubeSize
+    } doshins!`
+  );
+  console.log(extraPics);
+
+  id.innerHTML += `<img class="doshin" src="../images/doshin${randNum}.png" alt=""/>`;
+
+  if (doshins == cubeSize * cubeSize) {
+    cubeSize++;
+    prevCubeSize++;
+  }
+
+  let height = prevCubeSize;
+  let width = 0;
+  let row = 1;
+  let rowSize = cubeSize;
+
+  document.querySelectorAll(".doshin").forEach((element) => {
+    currentDoshin++;
+
+    // if (height * )
+    if(currentDoshin > rowSize * row && ) rowSize;
+    else if (Math.ceil(doshins / height) > rowSize) rowSize++;
+
+    console.log(`THE ROW SIZE IS ${rowSize}`);
+
+    if (doshins % prevCubeSize == 0) {
+      console.log("PASS ONE");
+      width = doshins / prevCubeSize;
+    } else if (extraPics > 0 && currentDoshin >= rowSize * row) {
+      console.log("PASS TWO");
+      width = rowSize;
+      row++;
+      extraPics--;
+    } else {
+      console.log("PASS THREE");
+      width = rowSize;
+    }
+
+    // else if (extraPics > 0 && currentDoshin > cubeSize * row) {
+    //   width = cubeSize;
+    //   extraPics--;
+    //   row++;
+    // console.log("PASS ONE");
+    // } else if (extraPics > 0) {
+    //   console.log("PASS TWO");
+    //   width = cubeSize;
+    // } else {
+    //   console.log("PASS THREE");
+    //   width = prevCubeSize;
+    // }
+
+    width = element.style.setProperty("width", `${100 / width}%`); //TODO NEEDS CHANGING
+    element.style.setProperty("height", `${100 / height}%`); //! DONT CHANGE
+  });
+}
+
+// element.style.setProperty("width", `${100}%`);
